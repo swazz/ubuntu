c147f26ec9bb6f7b8d490a7867beaac4f0e34e7e
swathi <priyaswa14@gmail.com>
2018-09-25 10:37:47 +0100

    updated the popup for valid and invalid forms

--- client/src/containers/OrganisationsRegister/OrganisationsRegister.js
+++ client/src/containers/OrganisationsRegister/OrganisationsRegister.js
@@ -20,21 +20,29 @@ class OrganisationsRegister extends React.Component {
   //   });
   // }
 
+  
   onSubmit = e => {
     e.preventDefault();
+    this.setState({
+      formSubmitted: true
+    });
     console.log(this.state);
-    if (this.state.contactName.length === 0) {
+    if (this.state.contactName.length === 0 || this.state.email.length ===0 ) {
       // invalid
-      this.setState({
-        formValid: false
-      });
+      this.setState({ formValid: false });
     } else {
+      document.getElementById("organisation-form").reset();
       //valid
       this.setState({ formValid: true });
       api.saveUser(this.state).then(() => {
         this.props.history.push("/login");
       });
     }
+    
+  };
+
+  onClick = e => {
+    this.onSubmit(e);
   };
 
   onChange = e => {
@@ -44,40 +52,29 @@ class OrganisationsRegister extends React.Component {
   };
 
   render() {
-    return (
-      <div className="container">
+    return <div className="container">
         <div className="row">
           <div className="col-12 col-md-6 offset-md-3">
-            <form onSubmit={this.onSubmit}>
+            <form id="organisation-form" onSubmit={this.onSubmit}>
               <p className="h4 text-center mb-4">
-                Please provide all the information requested. Your registration
-                cannot be completed without it.
+                Please provide all the information requested. Your
+                registration cannot be completed without it.
               </p>
+              {this.state.formValid && this.state.formSubmitted ? <div class="alert alert-success" role="alert">
+                  <strong>Valid Form!</strong>
+                </div> : null}
+              {!this.state.formValid && this.state.formSubmitted ? <div class="alert alert-danger" role="alert">
+                  <strong>Invalid Form</strong>
+                </div> : null}
               <div className="form-group">
                 <label htmlFor="contactName" className="sr-only">
                   Your name
                 </label>
-                <input
-                  type="text"
-                  name="contactName"
-                  onChange={this.onChange}
-                  id="contactName"
-                  placeholder="Your name"
-                  className="form-control"
-                  required
-                />
+                <input type="text" name="contactName" onChange={this.onChange} id="contactName" placeholder="Your name" className="form-control" />
               </div>
               <div className="form-group">
                 <label htmlFor="organisationName" className="sr-only" />
-                <input
-                  type="text "
-                  id="organisationName"
-                  name="organisationName"
-                  onChange={this.onChange}
-                  placeholder="Organisation name"
-                  className="form-control"
-                  required
-                />
+                <input type="text " id="organisationName" name="organisationName" onChange={this.onChange} placeholder="Organisation name" className="form-control" />
                 <div class="invalid-feedback">
                   Please provide your organisation name
                 </div>
@@ -86,60 +83,27 @@ class OrganisationsRegister extends React.Component {
                 <label htmlFor="address" className="sr-only">
                   Address
                 </label>
-                <input
-                  type="text"
-                  id="address"
-                  name="address"
-                  onChange={this.onChange}
-                  placeholder="Address"
-                  className="form-control"
-                  required
-                />
+                <input type="text" id="address" name="address" onChange={this.onChange} placeholder="Address" className="form-control" />
               </div>
               <div className="form-group">
                 <label htmlFor="email" className="sr-only">
                   Email
                 </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  onChange={this.onChange}
-                  placeholder="Email"
-                  className="form-control"
-                />
+                <input type="email" id="email" name="email" onChange={this.onChange} placeholder="Email" className="form-control" />
               </div>
               <div className="form-group">
                 <label htmlFor="phoneNumber" className="sr-only">
                   Phone number
                 </label>
-                <input
-                  type="number"
-                  id="number"
-                  name="phoneNumber"
-                  onChange={this.onChange}
-                  placeholder="Phone number"
-                  className="form-control"
-                />
+                <input type="number" id="number" name="phoneNumber" onChange={this.onChange} placeholder="Phone number" className="form-control" />
               </div>
               <div className="form-group">
                 <label htmlFor="password" className="sr-only">
                   Password
                 </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  onChange={this.onChange}
-                  placeholder="Password"
-                  className="form-control"
-                />
+                <input type="password" id="password" name="password" onChange={this.onChange} placeholder="Password" className="form-control" />
               </div>
-              {!this.state.formValid ? (
-                <div class="alert alert-danger" role="alert">
-                  Form is not valid
-                </div>
-              ) : null}
+
               <div className="text-center mt-4">
                 <button className="btn btn-large btn-primary" type="submit">
                   Register
@@ -148,8 +112,7 @@ class OrganisationsRegister extends React.Component {
             </form>
           </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 export default OrganisationsRegister;
